de0bfb3b0f9e052bab95c13390e32e2b214a933d
Souriya Roy <souriyaroy1998@gmail.com>
2021-06-06 09:44:01 +0530

    Added loader

--- src/app/prac-form/prac-form.module.ts
+++ src/app/prac-form/prac-form.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgxLoadingModule } from 'ngx-loading'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms'
-
+import {NgxSpinnerModule} from 'ngx-spinner'
 import { PracFormRoutingModule } from './prac-form-routing.module';
 import { BootFormComponent } from './boot-form/boot-form.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
@@ -14,7 +14,9 @@ import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
     CommonModule,
     PracFormRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxSpinnerModule,
+    NgxLoadingModule
   ]
 })
 export class PracFormModule { }
